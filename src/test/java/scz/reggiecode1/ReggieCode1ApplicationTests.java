eb9b847c0d8dfeb7d9182c29d9e75fc8d6b12532
scz-scz-scz <69664989@qq.com>
2025-06-22 11:28:52 +0800

    修改了yml中的path路径和test类

--- src/test/java/scz/reggiecode1/ReggieCode1ApplicationTests.java
+++ src/test/java/scz/reggiecode1/ReggieCode1ApplicationTests.java
@@ -8,13 +8,4 @@ import scz.reggiecode1.utils.ValidateCodeUtils;
 
 @SpringBootTest
 class ReggieCode1ApplicationTests {
-    @Autowired
-    private SMSUtils smsUtils;
-
-    @Test
-    public void SMSTest(){
-        String code= ValidateCodeUtils.generateValidateCode(2);
-        smsUtils.sendMessage("13187839614",code);
-        System.out.println(code);
-    }
 }
